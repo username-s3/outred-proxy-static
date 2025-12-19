41b0da08040411c67842bcbe412bca66a2ac3a07
username-s3 <Administrator@elusion.website>
2025-12-19 10:12:06 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/uv/service/',
-    bare: '/bare/', // Это будет игнорироваться, так как работает Wisp через bare-mux
+    bare: '/bare/', 
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
